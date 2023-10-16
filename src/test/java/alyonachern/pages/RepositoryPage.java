62303569e6ae8a65e5c428fc97a99c06fbe1650f
alyonachern <alenaisakova@mail,ru>
2023-10-16 14:18:46 +0300

    remove import

--- src/test/java/alyonachern/pages/RepositoryPage.java
+++ src/test/java/alyonachern/pages/RepositoryPage.java
@@ -3,7 +3,6 @@ package alyonachern.pages;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
-import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static org.openqa.selenium.By.linkText;
